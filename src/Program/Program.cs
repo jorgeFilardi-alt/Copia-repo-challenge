3c00c9ace33948608d6c537bdddf02aeb9a631cb
jorgeFilardi-alt <jorgeandresfs01@gmail.com>
2024-08-12 10:33:53 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,10 +5,3 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-public class Division
-{
-    public static double Divide(int a, int b)
-    {
-        return (double)a / b;
-    }
-}
